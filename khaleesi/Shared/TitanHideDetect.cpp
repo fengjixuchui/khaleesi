4812197b4a8a2c0a7d339f908bbe3561af8daacb
lurumdare <matthew0haig@gmail.com>
2019-08-04 07:44:44 +0300

    Add TitanHide detect (testsign or debug mode)

---
+++ khaleesi/Shared/TitanHideDetect.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "TitanHideDetect.h"
+
+bool TitanHideCheck()
+{
+	HMODULE ntdll = GetModuleHandleA("ntdll.dll");
+
+	auto NtQuerySystemInformation = (t_NtQuerySystemInformation)GetProcAddress(ntdll, "NtQuerySystemInformation");
+
+	SYSTEM_CODEINTEGRITY_INFORMATION cInfo;
+	cInfo.Length = sizeof(cInfo);
+
+	NtQuerySystemInformation(
+		SystemCodeIntegrityInformation, // id получения информации о CodeIntegrity
+		&cInfo,
+		sizeof(cInfo),
+		NULL
+	);
+
+	return (cInfo.CodeIntegrityOptions & CODEINTEGRITY_OPTION_TESTSIGN)
+		|| (cInfo.CodeIntegrityOptions & CODEINTEGRITY_OPTION_DEBUGMODE_ENABLED);
+}
---
+++ khaleesi/Shared/TitanHideDetect.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <Windows.h>
+#include <iostream>
+
+#define CODEINTEGRITY_OPTION_TESTSIGN 0x00000002
+#define CODEINTEGRITY_OPTION_DEBUGMODE_ENABLED 0x00000080
+
+#define SystemCodeIntegrityInformation 0x67
+
+typedef struct _SYSTEM_CODEINTEGRITY_INFORMATION {
+	ULONG Length;
+	ULONG CodeIntegrityOptions;
+} SYSTEM_CODEINTEGRITY_INFORMATION;
+
+typedef NTSTATUS(__stdcall* t_NtQuerySystemInformation)(
+	IN ULONG,
+	OUT PVOID,
+	IN ULONG,
+	OUT PULONG
+	);
+
+
+bool TitanHideCheck();
